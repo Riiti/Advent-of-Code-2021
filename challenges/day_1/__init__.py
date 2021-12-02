be47b6599f69c3e47d85ac99930a0165a0ea8bc5
Niklas Rittmann <niklas.rittmann@me.com>
2021-12-02 10:46:51 +0100

    Initial Commit

---
+++ challenges/day_1/__init__.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+from pathlib import Path
+
+
+class Sonar:
+    def __init__(self, array:np.array) -> None:
+        self.values = array
+
+    def get_num_of_larger(self)->int:
+        return np.sum(np.where(self.values[:-1]<self.values[1:], 1, 0))
+
+    def larger_in_window_of_three(self)->int:
+        window_sum = self.values[:-2]+ self.values[1:-1] + self.values[2:]
+        return np.sum(np.where(window_sum[:-1]<window_sum[1:], 1, 0))
+
+    @classmethod
+    def read_file(cls):
+   
+        path = f'{Path(__file__).parent}/data/sonar_values.txt'
+        with open(path) as f:
+            data = [int(line) for line in f.readlines()]
+        return cls(np.array(data))
+
+
