cb58ffc815d63fe21a8bb96c44f1bcf90277dd4d
Niklas Rittmann <niklas.rittmann@me.com>
2021-12-11 15:29:15 +0100

    chore: Updated isort and black

--- aoc/main.py
+++ aoc/main.py
@@ -1,3 +1,16 @@
-from aoc import Sonar
+# fmt: off
+from aoc import (Bingo, Command, Diagnostik, Digits, Lanternfish, Lava, Sonar,
+                 Syntax, Vents, Whales)
+
+# fmt: on
 
 Sonar.run()
+Command.run()
+Diagnostik.run()
+Bingo.run()
+Vents.run()
+Lanternfish.run()
+Whales.run()
+Digits.run()
+Lava.run()
+Syntax.run()
